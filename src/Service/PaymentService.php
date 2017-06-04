6969cc6fdf3ace185d2754202b9928aa530a7587
Dennis de Greef <github@link0.net>
2017-06-04 17:25:37 +0200

    Implement PaymentService::pay

--- src/Service/PaymentService.php
+++ src/Service/PaymentService.php
@@ -3,9 +3,11 @@
 namespace Link0\Bunq\Service;
 
 use Link0\Bunq\Client;
+use Link0\Bunq\Domain\Alias;
 use Link0\Bunq\Domain\Id;
 use Link0\Bunq\Domain\Payment;
 use Link0\Bunq\Domain\User;
+use Money\Money;
 
 final class PaymentService
 {
@@ -44,5 +46,49 @@ final class PaymentService
             '/monetary-account/' . $this->monetaryAccountId .
             '/payment'
         );
+    }
+
+    /**
+     * This method performs a payment and returns a payment Id
+     *
+     * @param Money $money
+     * @param Alias $alias
+     * @param string $description
+     * @param Id[] $attachments,
+     * @param string $merchantReference
+     * @return Id
+     */
+    public function pay(
+        Money $money,
+        Alias $alias,
+        string $description,
+        array $attachments = [],
+        string $merchantReference = ''
+    ): Id {
+        $data = [
+            'money' => [
+                'amount' => $money->getAmount(),
+                'currency' => $money->getCurrency()->getCode(),
+            ],
+            'counterparty_alias' => [
+                'type' => $alias->type(),
+                'value' => $alias->value(),
+                'name' => $alias->name(),
+            ],
+            'description' => $description,
+            'merchant_reference' => $merchantReference,
+        ];
+
+        foreach ($attachments as $attachment) {
+            // Currently an array of Ids
+            $data['attachment'][]['id'] = $attachment;
+        }
+
+        return $this->client->post(
+            'user/' . $this->userId .
+            '/monetary-account/' . $this->monetaryAccountId .
+            '/payment',
+            $data
+        )['Id'];
     }
 }
